0f28b56bafd836b2480c9c0f27ae43ebc9fcb69a
sonal bhanawat <sonalbhanawat@yahoo.com>
2016-06-17 13:20:04 +0530

    adding project structure to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brochure::Application.config.secret_token = 'e306418f12583a452e9812e3fbd8b347a629b86af6f892c7ba00d1615fe46098fca5245622d220be9ccee68fbfa0f35a7c698e32a4ea605783cee0097cc123b6'
